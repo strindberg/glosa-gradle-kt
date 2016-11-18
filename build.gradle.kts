848332fecfe884784195f61c23c24eeb831e16ac
Jesper Holmberg <jesper.holmberg@callistaenterprise.se>
2016-11-18 15:59:12 +0100

    Första försök med kotlin-gradle, saknad fil

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+import org.gradle.api.JavaVersion.VERSION_1_8
+
+group = "se.jh"
+version = "1.0-SNAPSHOT"
+
+apply<ApplicationPlugin>()
+
+configure<ApplicationPluginConvention> {
+    mainClassName = "se.jh.glosa.fw.Glosa"
+}
+
+// configure<JavaPluginConvention> {
+//     sourceCompatibility = VERSION_1_8
+//     targetCompatibility = VERSION_1_8
+// }
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    testCompile("junit:junit:4.12")
+}
